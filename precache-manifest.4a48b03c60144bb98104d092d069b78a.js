ecc83aeed9b9c24ede71c5dd8113f47eee36ebca
deanb <62507744+deanblinder@users.noreply.github.com>
2020-10-16 14:49:30 +0300

    Updates

---
+++ precache-manifest.4a48b03c60144bb98104d092d069b78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d932e09df0aa5754dc674d1134c6034",
+    "url": "/buyTheWay/index.html"
+  },
+  {
+    "revision": "8e62936fdb66549f86ad",
+    "url": "/buyTheWay/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d8fb12e1ab7c026f0e5",
+    "url": "/buyTheWay/static/js/2.558406de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/buyTheWay/static/js/2.558406de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e62936fdb66549f86ad",
+    "url": "/buyTheWay/static/js/main.3074003e.chunk.js"
+  },
+  {
+    "revision": "f4f33e148673f228d7a6",
+    "url": "/buyTheWay/static/js/runtime-main.a4765aec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buyTheWay/static/media/logo.5d5d9eef.svg"
+  }
+]);
